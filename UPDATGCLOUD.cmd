450ffd9a970ac7c675a1d594be48adc8c3306582
lukaskiermeyer <80913379+lukaskiermeyer@users.noreply.github.com>
2026-01-29 17:43:22 +0100

    .

--- UPDATGCLOUD.cmd
+++
@@ -1,4 +0,0 @@
-
-docker build -t gcr.io/strapi-website-454221/strapi-dorfbuehne:latest . 
-docker buildx build -t gcr.io/strapi-website-454221/strapi-dorfbuehne:latest .
-docker push gcr.io/strapi-website-454221/strapi-dorfbuehne:latest
